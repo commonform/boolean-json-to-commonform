37937c5a630974db56cf5b5cd3b6a3ef72c5c76c
Kyle E. Mitchell <kyle@kemitchell.com>
2016-08-21 14:51:50 -0700

    Use JavaScript Standard Style

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-module.exports = booleanJSONToCommonForm
-
-function booleanJSONToCommonForm(argument) {
+module.exports = function booleanJSONToCommonForm (argument) {
   if (Array.isArray(argument)) {
-    return { content: argument } }
-  else if ('and' in argument) {
+    return {content: argument}
+  } else if ('and' in argument) {
     return {
-      content: [ 'all of these are the case:' ]
-        .concat(
-          argument.and.map(function(conjunct) {
-            return {
-              form: booleanJSONToCommonForm(conjunct) } })) } }
-  else if ('or' in argument) {
+      content: ['all of these are the case:']
+      .concat(
+        argument.and.map(function (conjunct) {
+          return {form: booleanJSONToCommonForm(conjunct)}
+        })
+      )
+    }
+  } else if ('or' in argument) {
     return {
-      content: [ 'any of these is the case:' ]
-        .concat(
-          argument.or.map(function(disjunct) {
-            return {
-              form: booleanJSONToCommonForm(disjunct) } })) } }
-  else if ('not' in argument) {
+      content: ['any of these is the case:']
+      .concat(
+        argument.or.map(function (disjunct) {
+          return {form: booleanJSONToCommonForm(disjunct)}
+        })
+      )
+    }
+  } else if ('not' in argument) {
     return {
-      content: [ 'it is not the case that ' ]
-        .concat(booleanJSONToCommonForm(argument.not).content) } }
-  else {
-    throw new Error('Invalid input') } }
+      content: ['it is not the case that ']
+      .concat(booleanJSONToCommonForm(argument.not).content)
+    }
+  } else {
+    throw new Error('Invalid input')
+  }
+}
